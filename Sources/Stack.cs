eea820b4af43600c97a6695b70a77c7bdcd9889a
Marcos Lopez C <MarcosLopezC@live.com>
2014-04-28 20:32:20 -0400

    Adding Stack class.

---
+++ Sources/Stack.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace ArtEvolver
+{
+	public class Stack
+	{
+		public const int MaxSize = 1000;
+
+		private const int MaxIndex = MaxSize - 1;
+
+		private const double DefaultValue = 0.0;
+
+		private int index = 0;
+
+		private double[] values;
+
+		public double Top
+		{
+			get { return Peek(); }
+		}
+
+		public int Count
+		{
+			get { return index; }
+		}
+
+		public Stack()
+		{
+			values = new double[MaxSize];
+		}
+
+		public void Push(double value)
+		{
+			values[index] = value;
+
+			if (index < MaxIndex)
+			{
+				index += 1;
+			}
+		}
+
+		public double Pop()
+		{
+			if (index > 0)
+			{
+				index -= 1;
+
+				return values[index];
+			}
+			else
+			{
+				return DefaultValue;
+			}
+		}
+
+		public double Peek()
+		{
+			if (index > 0)
+			{
+				return values[index - 1];
+			}
+			else
+			{
+				return DefaultValue;
+			}
+		}
+
+		public void Clear()
+		{
+			index = 0;
+		}
+
+		public override string ToString()
+		{
+			return string.Format("Top = {0} Count = {1}", Top, Count);
+		}
+	}
+}
