9cdf5f05eb0e357eb1d2e862bd93820bc3c4fe00
Marcos Lopez C <MarcosLopezC@live.com>
2014-08-16 22:08:43 -0400

    Adding parsing and serializing functionality.

--- Sources/VirtualMachine/Program.cs
+++ Sources/VirtualMachine/Program.cs
@@ -19,5 +19,77 @@ namespace ArtEvolver.VirtualMachine
 			Data       = new List<double>(1000);
 			StackSize  = 1000;
 		}
+
+		public string SerializeOperations()
+		{
+			var output = new StringBuilder();
+
+			for (var i = 0; i < Operations.Count; i += 1)
+			{
+				output.AppendFormat("{0};\n", Enum.GetName(typeof(Operation), Operations[i]));
+			}
+
+			return output.ToString();
+		}
+
+		public string SerializeData()
+		{
+			var output = new StringBuilder();
+
+			for (var i = 0; i < Data.Count; i += 1)
+			{
+				output.AppendFormat("{0};\n", Data[i]);
+			}
+
+			return output.ToString();
+		}
+
+		public void ParseOperations(string input)
+		{
+			var operationNames = input.Split(';');
+
+			foreach (var operationName in operationNames)
+			{
+				if (string.IsNullOrWhiteSpace(operationName))
+				{
+					continue;
+				}
+
+				Operation operationValue;
+
+				if (Enum.TryParse<Operation>(operationName, out operationValue))
+				{
+					this.Operations.Add(operationValue);
+				}
+				else
+				{
+					throw new Exception("Invalid operation.");
+				}
+			}
+		}
+
+		public void ParseData(string input)
+		{
+			var dataSet = input.Split(';');
+
+			foreach (var valueString in dataSet)
+			{
+				if (string.IsNullOrWhiteSpace(valueString))
+				{
+					continue;
+				}
+
+				double value;
+
+				if (double.TryParse(valueString, out value))
+				{
+					this.Data.Add(value);
+				}
+				else
+				{
+					throw new Exception("Invalid data value.");
+				}
+			}
+		}
 	}
 }
