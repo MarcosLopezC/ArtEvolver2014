5ee7a09d5b5c48868b63d61503b4ffe3c09cedcc
Marcos Lopez C <MarcosLopezC@live.com>
2014-08-19 19:21:42 -0400

    Adding MathUtility class.

---
+++ Sources/MathUtility.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArtEvolver
+{
+	public static class MathUtility
+	{
+		public const double Tau = Math.PI * 2;
+
+		public static double Normalize(double value, double min, double max)
+		{
+			return (value - min) / (max - min);
+		}
+
+		public static double LinearInterpolation(double value, double min, double max)
+		{
+			return (1 - value) * min + value * max;
+		}
+
+		public static double Map(double value, double fromMin, double fromMax, double toMin, double toMax)
+		{
+			var normal = Normalize(value, fromMin, fromMax);
+			return LinearInterpolation(normal, toMin, toMax);
+		}
+
+		public static double Constrain(double value, double min, double max)
+		{
+			return Math.Min(Math.Max(value, min), max);
+		}
+
+		public static int Constrain(int value, int min, int max)
+		{
+			return Math.Min(Math.Max(value, min), max);
+		}
+	}
+}
