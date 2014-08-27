2acd883f14332eda258061516c7a3f408f980d25
Marcos Lopez C <marcoslopezc@live.com>
2014-08-27 12:17:24 -0400

    Adding Region class.

---
+++ Sources/Rendering/Region.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArtEvolver.Rendering
+{
+	public class Region
+	{
+		public double X { get; set; }
+		public double Y { get; set; }
+
+		public double Width { get; set; }
+		public double Height { get; set; }
+
+		public bool IsEmpty
+		{
+			get
+			{
+				return Width == 0 && Height == 0;
+			}
+		}
+
+		public Region(double width, double height)
+		{
+			this.Width  = width;
+			this.Height = height;
+		}
+
+		public Region(double width, double height, double x, double y) : this(width, height)
+		{
+			this.X = x;
+			this.Y = y;
+		}
+	}
+}
