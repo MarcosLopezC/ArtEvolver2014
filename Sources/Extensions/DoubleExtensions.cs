76b083030ef4e80f6cf656a44fd56abd65049e62
Marcos Lopez C <MarcosLopezC@live.com>
2014-09-01 19:00:25 -0400

    Adding double extension methods.

---
+++ Sources/Extensions/DoubleExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArtEvolver.Extensions
+{
+	public static class DoubleExtensions
+	{
+		public static bool IsNumber(this double value)
+		{
+			return MathUtility.IsNumber(value);
+		}
+
+		public static bool IsNaN(this double value)
+		{
+			return double.IsNaN(value);
+		}
+	}
+}
