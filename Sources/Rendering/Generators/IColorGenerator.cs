c80ca797f8cb0ba7807f33c2ef0a4d601eb843ac
Marcos Lopez C <MarcosLopezC@live.com>
2014-09-01 20:38:08 -0400

    Adding IColorGenerator interface.

---
+++ Sources/Rendering/Generators/IColorGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArtEvolver.Rendering.Generators
+{
+	public interface IColorGenerator
+	{
+		Color Generate(double value);
+	}
+}
