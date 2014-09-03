438bc9076071ab2880a4c0a5c6c0fbfe99ef1b45
Marcos Lopez C <MarcosLopezC@live.com>
2014-09-02 20:51:16 -0400

    Adding UniformGenerator class.

---
+++ Sources/Rendering/Generators/UniformGenerator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ArtEvolver.Extensions;
+
+namespace ArtEvolver.Rendering.Generators
+{
+	public class UniformGenerator : IColorGenerator
+	{
+		public Color UndefinedColor { get; set; }
+
+		public double HueScale { get; set; }
+
+		public double HueOffset { get; set; }
+
+		public double SaturationScale { get; set; }
+
+		public double SaturationOffset { get; set; }
+
+		public double BrightnessScale { get; set; }
+
+		public double BrightnessOffset { get; set; }
+
+		public UniformGenerator()
+		{
+			HueScale        = 1;
+			SaturationScale = 1;
+			BrightnessScale = 1;
+		}
+
+		public Color Generate(double value)
+		{
+			if (value.IsNumber())
+			{
+				return Color.FromHsb(
+					MathUtility.Mod(HueOffset + HueScale * value, Color.MaxHue),
+					MathUtility.TriangularMod(SaturationOffset + SaturationScale * value, Color.MaxSaturation),
+					MathUtility.TriangularMod(BrightnessOffset + BrightnessScale * value, Color.MaxBrightness)
+				);
+			}
+			else
+			{
+				return UndefinedColor;
+			}
+		}
+	}
+}
